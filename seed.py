4767468658a9db641fe50a2992e7346d7340eade
htkaya07 <47039316+htkaya07@users.noreply.github.com>
2021-07-25 17:54:44 -0400

    seed database tables

--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 """Seed database with sample data from CSV Files."""
 
-from csv import DictReader
+
 from app import db
 from models import User, Message, Follows
 
@@ -8,13 +8,6 @@ from models import User, Message, Follows
 db.drop_all()
 db.create_all()
 
-with open('generator/users.csv') as users:
-    db.session.bulk_insert_mappings(User, DictReader(users))
 
-with open('generator/messages.csv') as messages:
-    db.session.bulk_insert_mappings(Message, DictReader(messages))
 
-with open('generator/follows.csv') as follows:
-    db.session.bulk_insert_mappings(Follows, DictReader(follows))
 
-db.session.commit()
