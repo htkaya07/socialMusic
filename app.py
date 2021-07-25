98a1fddb0f83c3be93066eb7c07854aec40112a3
htkaya07 <47039316+htkaya07@users.noreply.github.com>
2021-07-25 17:27:23 -0400

    add heroku psql url

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgres:///socialmusic'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'SECRET')
 #toolbar = DebugToolbarExtension(app)
 
